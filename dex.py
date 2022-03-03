aded1bbd66b45bc5ea4519d10d5ce10643d779de
llamas-are-awesome <99903671+llamas-are-awesome@users.noreply.github.com>
2022-03-03 14:18:31 -0800

    Update dex.py

--- dex.py
+++ dex.py
@@ -5,54 +5,23 @@ import pprint
 
 ch_stat = {}
 stat= input("Which stat? HP, Attack, Defense, SP Atk, SP Def, or Speed?: ")
+
 with open("pokedex.txt", "r") as dexfile:
 
+        pokedata= csv.DictReader(dexfile)
 
-        if stat.lower() == "hp":
+        for line in pokedata:
             try:
-                for line in dexfile:
-                    (key, value) = line.split(",")[1],int(line.split(",")[5])
-                    ch_stat[key] = value
-            except ValueError:
+
+                newdict= {line["Name"]: line[stat]}
+
+                ch_stat.update(newdict)
+            except:
                 pass
-        #print(ch_stat)
-            #print(line.split(",")[1],line.split(",")[5])
-        elif stat.lower() == "attack":
-            for line in dexfile:
-                (key, value) = line.split(",")[1],line.split(",")[6]
-                ch_stat[key] = value
-        #print(ch_stat)
 
-        elif stat.lower() == "defense":
-            for line in dexfile:
-                (key, value) = line.split(",")[1],line.split(",")[7]
-                ch_stat[key] = value
-        #print(ch_stat)
-
-        elif stat.lower() == "sp attack" or "sp atk":
-            for line in dexfile:
-                (key, value) = line.split(",")[1],line.split(",")[8]
-                ch_stat[key] = value
-        #print(ch_stat)
-
-        elif stat.lower() == "sp def" or "sp defense":
-            for line in dexfile:
-                (key, value) = line.split(",")[1],line.split(",")[9]
-                ch_stat[key] = value
-        #print(ch_stat)
-
-        elif stat.lower() == "speed":
-            for line in dexfile:
-                (key, value) = line.split(",")[1],line.split(",")[10]
-                ch_stat[key] = value
-        #print(ch_stats)
-
-        else:
-            print("learn to read")
-
-
+    
 sorted_stat = (sorted( ((v,k) for k,v in ch_stat.items())))
 pprint.pprint(sorted_stat)
 
 with open("sorteddex.txt", "w") as text_file:
-    pprint.pprint(sorted_stat)
+    pprint.pprint(sorted_stat, text_file)
